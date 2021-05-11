8d9cd8fbe4ec54c2f35ab2f11fc63cf76700e2c8
humblEgo <injun.woo30000@gmail.com>
2021-05-12 03:59:35 +0900

    id, name, guild 정보를 맵핑하기 위해 전역해쉬 사용

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, session, request
+
+id_by_name = {}
+guild_by_id = {}
+
 app = Flask(__name__)
 app.secret_key = 'app secret key'
 import os
 BROTHER_IP = os.environ["BROTHER_IP"]
+
 
 @app.route("/")
 def home():
@@ -15,22 +20,53 @@ def print_test():
     import json
     from datetime import datetime
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((BROTHER_IP,9100))
-        printjob = BrotherPrint(s)
-        printjob.template_mode()
-        printjob.template_init()
-        printjob.select_and_insert("id", request.args.get('id'))
-        printjob.select_and_insert("group", request.args.get('guild'))
-        printjob.template_print()
+    try:
+        user_id = id_by_name[request.args.get('name')]
+        guild_name = guild_by_id[user_id]
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((BROTHER_IP,9100))
+            printjob = BrotherPrint(s)
+            printjob.template_mode()
+            printjob.template_init()
+            printjob.select_and_insert("id", request.args.get('name'))
+            printjob.select_and_insert("group", request.args.get('guild'))
+            printjob.template_print()
+        return render_template("index.html")
+    except:
+        return render_template("index.html", invalid_name=True)
+
+@app.route("/init")
+def map_data():
+    map_id_and_name()
+    map_id_and_guild("42data/gun")
+    map_id_and_guild("42data/gon")
+    map_id_and_guild("42data/gam")
+    map_id_and_guild("42data/lee")
+    
     return render_template("index.html")
 
 
-#@app.route("/print", methods=['GET'])
-def test():
-    print("test")
-    return ('test')
+def map_id_and_name():
+    file = open("42data/id_and_intra_name", "r")
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        user_id, user_name = line.strip().split(sep=":")
+        id_by_name[user_name] = user_id
+    file.close()
 
+
+def map_id_and_guild(path):
+    file = open(path, "r")
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        user_id, guild = line.strip().split(sep=":")
+        guild_by_id[user_id] = guild
+    file.close()
+    
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
